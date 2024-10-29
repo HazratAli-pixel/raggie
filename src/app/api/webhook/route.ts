bc2148a577f7b1e107e32e26681501a29a9336e1
hazrat369ali@gmail.com <hazrat369ali@gmail.com>
2024-10-29 12:25:33 +0600

    openai key change

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,35 +1,37 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-// async function getOpenAIResponse(userMessage: string) {
-//   const response = await axios.post(
-//     "https://api.openai.com/v1/chat/completions",
-//     {
-//       model: "gpt-3.5-turbo",
-//       messages: [{ role: "user", content: userMessage }],
-//     },
-//     {
-//       headers: {
-//         Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
-//       },
-//     }
-//   );
-//   return response.data.choices[0].message.content;
-// }
+async function getOpenAIResponse(userMessage: string) {
+  const response = await axios.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-4o",
+      messages: [{ role: "user", content: userMessage }],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
+      },
+    }
+  );
+  return response.data.choices[0].message.content;
+}
 
 export async function POST(req: Request) {
   const payload = await req.json();
   console.log("Payload :", payload);
   console.log("Request: ", req);
   if (req.body) {
-    // const userMessage: string = await payload.body.events[0].message.text;
-    // const userMessage: string = "what is the capital of bangladesh";
-    // const openAIResponse = await getOpenAIResponse(userMessage);
+    const userMessage: string = await payload.events[0].message.text;
+    const openAIResponse = await getOpenAIResponse(userMessage);
+    console.log("OpenAI Response: ", openAIResponse);
     await axios.post(
       `https://api.line.me/v2/bot/message/reply`,
       {
         replyToken: payload.events[0].replyToken,
-        messages: [{ type: "text", text: "Reply from api Raggie -s" }],
+        messages: [
+          { type: "text", text: `Reply from Raggie api  ${openAIResponse} ` },
+        ],
       },
       {
         headers: {
@@ -40,7 +42,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({
       statusCode: 200,
-      // openAIResponse: openAIResponse,
     });
   }
 }
