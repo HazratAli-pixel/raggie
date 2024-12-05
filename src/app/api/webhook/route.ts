a49d0879e90edbee3d0a5850c0b9a2337aa77be9
hazrat369ali@gmail.com <hazrat369ali@gmail.com>
2024-12-05 11:02:27 +0600

    api modification

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -20,7 +20,6 @@ async function getOpenAIResponse(userMessage: string) {
 export async function POST(req: Request) {
   const rowBody = await req.text();
   const signature = req.headers.get("x-line-signature");
-  console.log("Body: ", rowBody);
   if (!signature) {
     return NextResponse.json({ error: "Missing signature" }, { status: 400 });
   }
@@ -28,22 +27,18 @@ export async function POST(req: Request) {
     .createHmac("sha256", "bed207df519919d69768f3190d0b2565")
     .update(rowBody)
     .digest("base64");
-  console.log("hash: ", hash);
-  console.log("signature: ", signature);
 
   if (hash !== signature) {
     return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
   }
   const payload = await JSON.parse(rowBody);
-  // const payload = await req.json();
-  console.log("Payload :", payload);
-  console.log("Request: ", req);
 
   if (req.body) {
     const userMessage: string = await payload.events[0].message.text;
+    const userMessages = userMessage.replace(/<@[^>]+>/, "").trim();
     const mentioned = userMessage.includes("@ALEX");
-    console.log("Mention", mentioned, userMessage);
-    const openAIResponse = await getOpenAIResponse(userMessage);
+    console.log("Mention", mentioned, userMessage, userMessages);
+    const openAIResponse = await getOpenAIResponse(userMessages);
     if (mentioned) {
       await axios.post(
         `https://api.line.me/v2/bot/message/reply`,
