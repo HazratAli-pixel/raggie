0706b76c95f6fd1785d665f307e86ea4e8f813d8
hazrat369ali@gmail.com <hazrat369ali@gmail.com>
2024-12-05 09:23:54 +0600

    api modification

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
+import crypto from "crypto";
 import { NextResponse } from "next/server";
-
 async function getOpenAIResponse(userMessage: string) {
   const response = await axios.post(
     "https://api.openai.com/v1/chat/completions",
@@ -21,24 +21,23 @@ export async function POST(req: Request) {
   const payload = await req.json();
   console.log("Payload :", payload);
   console.log("Request: ", req);
-  const userMessages: string = await payload.events[0].message.text;
-  console.log("userMessages: ", userMessages);
   const signature = req.headers.get("x-line-signature");
-  console.log("signature: ", signature);
-  console.log("req headers: ", req.headers);
-  const rawBody = await req.text();
-  console.log("rawBody: ", rawBody);
+  // const rawBody = await req.text();
+  // console.log("rawBody: ", rawBody);
 
-  // if (!signature) {
-  //   return NextResponse.json({ error: "Missing signature" }, { status: 400 });
-  // }
-  // const hash = crypto
-  //   .createHmac("sha256", process.env.NEXT_PUBLIC_LINE_CHANNEL_ACCESS_TOKEN!)
-  //   .update(rawBody)
-  //   .digest("base64");
-  // if (hash !== signature) {
-  //   return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
-  // }
+  if (!signature) {
+    return NextResponse.json({ error: "Missing signature" }, { status: 400 });
+  }
+  const hash = crypto
+    .createHmac("sha256", process.env.NEXT_PUBLIC_LINE_CHANNEL_ACCESS_TOKEN!)
+    .update(payload)
+    .digest("base64");
+  console.log("hash: ", hash);
+  console.log("signature: ", signature);
+
+  if (hash !== signature) {
+    return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+  }
 
   if (req.body) {
     const userMessage: string = await payload.events[0].message.text;
