496e3909abf9632d83f334b82c06515629a50943
hazrat369ali@gmail.com <hazrat369ali@gmail.com>
2024-12-06 10:29:15 +0600

    api modification

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -43,9 +43,9 @@ export async function POST(req: Request) {
     console.log("Info", mentioned, userMessage, userMessages, mentionsWord[0]);
     const openAIResponse = await getOpenAIResponse(userMessages);
     if (mentioned) {
-      console.log("source", payload.events[0].message.source);
-      const user = await payload.events[0].message.source.userId;
-      console.log("User ID", payload.events[0].message.source.userId);
+      console.log("source", payload.events[0].source);
+      const user = await payload.events[0].source.userId;
+      console.log("User ID", payload.events[0].source.userId);
       await axios.post(
         `https://api.line.me/v2/bot/message/reply`,
         {
